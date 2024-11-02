48f0f6e213e2afbe1010547821ca8659f3e00da2
Armand Fouquiau <armand.fouquiau@gmail.com>
2024-11-02 22:51:31 +0100

    yolo

---
+++ 04_03_begin/landon-hotel/src/main/java/com/frankmoley/lil/landonhotel/web/controller/RoomController.java
@@ -0,0 +1,23 @@
+package com.frankmoley.lil.landonhotel.web.controller;
+
+import com.frankmoley.lil.landonhotel.data.repository.RoomRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rooms")
+public class RoomController {
+    public RoomController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    final private RoomRepository roomRepository;
+    @GetMapping
+    public String getRooms(Model model){
+        model.addAttribute("rooms", this.roomRepository.findAll());
+        return "room-list";
+
+    }
+}
